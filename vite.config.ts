832fd7d948e3ea20b787c039c5d738da77de1194
Alpacorp <alejandro.palacios88@gmail.com>
2022-12-08 11:29:19 -0500

    setup vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  appType: "mpa",
+  server: {
+    port: 3000, // El puerto en el que se ejecutará el servidor
+    browser: "true", // No se abrirá un navegador al iniciar el servidor
+    historyFallback: true, // Habilitar el historial de navegación
+    open: true, // No se abrirá un navegador al iniciar el servidor
+    spa: "client", // Configurar el servidor como una aplicación de una sola página (SPA)
+    static: "dist", // La carpeta que contiene el código estático de la aplicación
+  },
 });
