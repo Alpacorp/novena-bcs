94fe839bdc003b6d5ea09946e7063c7b8ceedc14
Alpacorp <alejandro.palacios88@gmail.com>
2022-12-08 11:39:56 -0500

    add configuration to vite setup file

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,16 @@ export default defineConfig({
     open: true, // No se abrirá un navegador al iniciar el servidor
     spa: "client", // Configurar el servidor como una aplicación de una sola página (SPA)
     static: "dist", // La carpeta que contiene el código estático de la aplicación
+    historyApiFallback: {
+      index: "/index.html", // El archivo que se servirá cuando se solicite una ruta que no existe
+
+      // Configurar las rutas que se deben ignorar
+      rewrites: [
+        {
+          from: /\/\/.*/,
+          to: (context) => context.parsedUrl.pathname,
+        },
+      ],
+    },
   },
 });
